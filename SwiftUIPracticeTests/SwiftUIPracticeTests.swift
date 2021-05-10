94c69bfdcfd4527271ba9801cbf8ae2aed51cc3d
Dharasis <b.dhara93@gmail.com>
2021-05-10 20:47:40 +0530

    + Add Unit test cases

---
+++ SwiftUIPracticeTests/SwiftUIPracticeTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftUIPracticeTests.swift
+//  SwiftUIPracticeTests
+//
+//  Created by dharasis behera on 10/05/21.
+//
+
+import XCTest
+
+class SwiftUIPracticeTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+      
+    }
+    
+    func testExample1(){
+        let string = "i am a test case"
+
+        XCTAssertEqual(string, "i am a test case")
+    }
+    func testExample2(){
+        let num = 2
+        let sq = num * num
+
+        XCTAssertEqual(sq, 4)
+    }
+
+}
